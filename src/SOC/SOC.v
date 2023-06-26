094f1eabd772f2568ab2fce84de7985e886e52db
Santiego <sandiegomail@qq.com>
2023-06-26 16:21:38 +0800

    finish ALU and add VERILOG assembler

--- src/SOC/SOC.v
+++ src/SOC/SOC.v
@@ -22,41 +22,40 @@ module SOC(
         .clk(clk),
         .resetn(resetn)
     );
-    reg [31:0] MEM [0:7];
-    integer i;
+    reg [31:0] MEM [0:255];
+    reg [31:0] PC;          // program counter
+    reg [31:0] instr;       // current instruction
+    `include "riscv_assembly.v"
+
     initial begin
         PC = 0;
-        // add x0, x0, x0
-        //                   rs2   rs1  add  rd   ALUREG
-        instr = 32'b0000000_00000_00000_000_00000_0110011;
-        // add x1, x0, x0
-        //                    rs2   rs1  add  rd  ALUREG
-        MEM[0] = 32'b0000000_00000_00000_000_00001_0110011;
-        // addi x1, x1, 1
-        //             imm         rs1  add  rd   ALUIMM
-        MEM[1] = 32'b000000000001_00001_000_00001_0010011;
-        // addi x1, x1, 1
-        //             imm         rs1  add  rd   ALUIMM
-        MEM[2] = 32'b000000000001_00001_000_00001_0010011;
-        // addi x1, x1, 1
-        //             imm         rs1  add  rd   ALUIMM
-        MEM[3] = 32'b000000000001_00001_000_00001_0010011;
-        // addi x1, x1, 1
-        //             imm         rs1  add  rd   ALUIMM
-        MEM[4] = 32'b000000000001_00001_000_00001_0010011;
-        // lw x2,0(x1)
-        //             imm         rs1   w   rd   LOAD
-        MEM[5] = 32'b000000000000_00001_010_00010_0000011;
-        // sw x2,0(x1)
-        //             imm   rs2   rs1   w   imm  STORE
-        MEM[6] = 32'b000000_00010_00001_010_00000_0100011;
-
-        // ebreak
-        //                                        SYSTEM
-        MEM[7] = 32'b000000000001_00000_000_00000_1110011;
+        ADD(x0,x0,x0);
+        ADD(x1,x0,x0);
+        ADDI(x1,x1,1);
+        ADDI(x1,x1,1);
+        ADDI(x1,x1,1);
+        ADDI(x1,x1,1);
+        ADD(x2,x1,x0);
+        ADD(x3,x1,x2);
+        SRLI(x3,x3,3);
+        SLLI(x3,x3,31);
+        SRAI(x3,x3,5);
+        SRLI(x1,x3,26);
+        EBREAK();
     end
 
-    reg [31:0] instr;
+    reg [31:0] RegisterBank [0:31];
+    `ifdef TEST_BENCH   
+    integer i;
+    initial begin
+        for(i=0; i<32; ++i) begin
+            RegisterBank[i] = 0;
+        end
+    end
+    `endif  
+    reg [31:0] rs1;
+    reg [31:0] rs2;
+
     wire isALUreg  =  (instr[6:0] == 7'b0110011); // rd <- rs1 OP rs2   
     wire isALUimm  =  (instr[6:0] == 7'b0010011); // rd <- rs1 OP Iimm
     wire isBranch  =  (instr[6:0] == 7'b1100011); // if(rs1 OP rs2) PC<-PC+Bimm
@@ -81,19 +80,46 @@ module SOC(
     wire [31:0] Bimm={{20{instr[31]}}, instr[7],instr[30:25],instr[11:8],1'b0};
     wire [31:0] Jimm={{12{instr[31]}}, instr[19:12],instr[20],instr[30:21],1'b0};
 
+    // ALU
+    wire [31:0] aluIn1 = rs1;
+    wire [31:0] aluIn2 = isALUreg ? rs2 : Iimm;
+    reg [31:0] aluOut;
 
-    reg [31:0] RegisterBank [0:31];
-    reg [31:0] rs1;
-    reg [31:0] rs2;
     wire [31:0] writeBackData;
     wire writeBackEn;
+    assign writeBackData = aluOut; 
+    assign writeBackEn = (state == EXECUTE && (isALUreg || isALUimm)); 
+
+    wire [4:0] shamt = isALUreg ? rs2[4:0] : instr[24:20]; // shift amount
+    
+    always @(*) begin // combinatorial block
+        case(funct3)
+            // SUB <- funct[7]==1 && instr[5]==1(ALUimm don't have SUB)
+            3'b000: aluOut = (funct7[5] & instr[5]) ? (aluIn1-aluIn2) : (aluIn1+aluIn2);
+            // left shift
+            3'b001: aluOut = aluIn1 << shamt;
+            // 	signed comparison (<)
+            3'b010: aluOut = ($signed(aluIn1) < $signed(aluIn2));
+            // unsigned comparison (<)
+            3'b011: aluOut = (aluIn1 < aluIn2);
+            // XOR
+            3'b100: aluOut = (aluIn1 ^ aluIn2);
+            // logical right shift(0) or arithmetic right shift(1)
+            3'b101: aluOut = funct7[5]? ($signed(aluIn1) >>> shamt) : (aluIn1 >> shamt); 
+            // OR
+            3'b110: aluOut = (aluIn1 | aluIn2);
+            // AND
+            3'b111: aluOut = (aluIn1 & aluIn2);	
+        endcase
+    end
+
+
 
     localparam FETCH_INSTR = 0;
     localparam FETCH_REGS  = 1;
     localparam EXECUTE     = 2;
     reg [1:0] state = FETCH_INSTR;
 
-    reg [4:0] PC;
     always @(posedge clk) begin
         if(!resetn) begin
             PC <= 0;
@@ -107,7 +133,7 @@ module SOC(
 
             case(state)
                 FETCH_INSTR: begin
-                    instr <= MEM[PC];
+                    instr <= MEM[PC[31:2]];
                     state <= FETCH_REGS;
                 end
                 FETCH_REGS: begin
@@ -116,7 +142,9 @@ module SOC(
                     state <= EXECUTE;
                 end
                 EXECUTE: begin
-                    PC <= PC + 1;
+                    if(!isSYSTEM) begin
+                        PC <= PC + 4;
+                    end
                     state <= FETCH_INSTR;	      
                 end
             endcase
